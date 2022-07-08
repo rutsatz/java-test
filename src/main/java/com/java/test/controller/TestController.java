94dd3b63dcadf261ad117535fcba7eea9315c125
Rafael Rutsatz <rafael.rutsatz@domo.com>
2022-07-08 11:39:02 -0300

    first commit

---
+++ src/main/java/com/java/test/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.java.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello! Time now is " + LocalTime.now();
+    }
+
+}
